00e44eb107453d6a2c7afbe4eeb2b25dd9057cea
Pegasusinnovate <pegasusinnovate@gmail.com>
2025-03-04 18:43:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_secret_key_for_dev')
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10 MB
+    MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50 MB
     # Path to your Firebase service account key JSON file
     FIREBASE_CREDENTIALS = os.environ.get('FIREBASE_CREDENTIALS')
     FIREBASE_STORAGE_BUCKET = os.environ.get('FIREBASE_STORAGE_BUCKET')
